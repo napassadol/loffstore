d19a9badc5123da4aac7411f0f94d060eb92ce00
napassadol <napassadol.s@gmail.com>
2018-06-07 20:24:58 +0700

    develop auth

--- static/Scripts/controller/loginCtrl.js
+++ static/Scripts/controller/loginCtrl.js
@@ -1,4 +1,28 @@
 'use strict';
-angular.module('app').controller('loginCtrl', function(){
-	console.log('loginCtrl');
-});
+angular.module('app').controller('loginCtrl', [ '$rootScope', '$scope', 'loginApi', 'SweetAlert', 
+	function($rootScope, $scope, loginApi, SweetAlert){
+		var vm = this
+
+		vm.submit = function(loginForm){
+			if(loginForm.$valid){
+				var request_data = {
+					'username' : vm.login.username,
+					'password' : vm.login.password
+				}
+				loginApi.login(request_data).then(
+					function successCallBack(response){
+						response = response.plain()
+						if(response.status == 'Success'){
+							$rootScope.auth = 
+
+							window.location.href = "/"
+						}
+						else{
+							SweetAlert.swal("login fail")
+						}
+					}
+				)
+			}
+		}
+	}
+]);
