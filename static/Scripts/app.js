139f68b5ec8de8bda3fee2d062ea68ef6e31b602
napassadol <napassadol.s@gmail.com>
2018-06-21 22:01:52 +0700

    FB connect

--- static/Scripts/app.js
+++ static/Scripts/app.js
@@ -89,3 +89,26 @@
         RestangularProvider.setBaseUrl('/');
     });
 
+    window.fbAsyncInit = function() {
+        FB.init({
+        appId      : '190074738365427',
+        cookie     : true,  // enable cookies to allow the server to access 
+                            // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v3.0' // use graph api version 2.8
+        });
+
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+            console.log(response);
+        });
+    };
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v3.0&appId=190074738365427&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+
